51f92e5c2d12e538c92e6f462c73e66d9f41e990
Sargis <s.avetisyan@medved.studio>
2020-03-18 17:22:18 +0300

    init

---
+++ function/FunctionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public string Handle(string input) 
+        {
+            Thread.Sleep(1000 * 15);
+
+            return $"Hi there - your input was: {input}\n";
+        }
+    }
+}
